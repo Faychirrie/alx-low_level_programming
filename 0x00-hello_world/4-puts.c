feda73d1c3c81839d7319841ccd1891993704967
Faychirrie <fkoech50@gmail.com>
2022-04-08 06:39:46 -0700

    message changes and comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-   /*
+/**
     * main - Entry point
     *
     * Return: Always 0 (Success)
     */
-
 int main(void)
 {
-  /* prints string using put */
   puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
 }
