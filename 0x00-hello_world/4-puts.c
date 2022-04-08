b870bdba8d1e3f96b4849d969a1e5968acc10439
Faychirrie <fkoech50@gmail.com>
2022-04-08 06:56:09 -0700

    remove trailing whitespaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 /**
-* main -Prints a value using puts   
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle"); 
